5fdc6639ae371fd8f4ddd3cf54abf518e1a5c98a
khrixtar-dev <khrixtar.gg@gmail.com>
2025-11-21 19:17:06 -0300

    Api productos creada

---
+++ src/main/java/cl/duocuc/huertohogar/backend/controller/ProductController.java
@@ -0,0 +1,70 @@
+package cl.duocuc.huertohogar.backend.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import cl.duocuc.huertohogar.backend.entity.Product;
+import cl.duocuc.huertohogar.backend.repository.ProductRepository;
+import cl.duocuc.huertohogar.backend.service.ProductService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "http://localhost:5173")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+    
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
+
+    @PostMapping()
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+   
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        return productRepository.findById(id)
+            .map(existingProduct -> {
+                existingProduct.setName(productDetails.getName());
+                existingProduct.setDescription(productDetails.getDescription());
+                existingProduct.setPrice(productDetails.getPrice());
+                existingProduct.setUrlImage(productDetails.getUrlImage());
+                existingProduct.setCategory(productDetails.getCategory());
+                Product updatedProduct = productRepository.save(existingProduct);
+                return ResponseEntity.ok(updatedProduct);
+            })
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+    }
+    
+
+}
---
+++ src/main/java/cl/duocuc/huertohogar/backend/service/ProductService.java
@@ -0,0 +1,32 @@
+package cl.duocuc.huertohogar.backend.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cl.duocuc.huertohogar.backend.entity.Product;
+import cl.duocuc.huertohogar.backend.repository.ProductRepository;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id){
+        return productRepository.findById(id);
+    }
+
+    public Product saveProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id){
+        productRepository.deleteById(id);
+    }
+}
